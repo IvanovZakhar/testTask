3df8e0c72f30dafadc5162c97ddc2de251cdbb0f
3axap <uvanov977777@gmail.com>
2022-07-06 21:57:40 +0300

    addEventListener

--- script.js
+++ script.js
@@ -52,4 +52,74 @@
         return result;
     }
 
+            // Создаем таблицу
+            let table = document.createElement('table');
+            let thead = document.createElement('thead');
+            let tbody = document.createElement('tbody');
+    
+            table.appendChild(thead);
+            table.appendChild(tbody);
+            document.body.appendChild(table);
+    
+    
+
+    class Courses {
+        constructor (name, prices, parentSelector, ...classes) {
+            this.name = name;
+            this.prices = prices;
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
+        }
+
+        render() {
+
+
+        const element = document.createElement('tr');
+        if(this.classes.length === 0){
+            this.classes = 'course__item';
+            element.classList.add(this.classes);
+        }else{
+             this.classes.forEach(className => element.classList.add(className));
+        }
+       
+        element.innerHTML = `
+                    <td class="user-id">
+                        ${this.name}
+                    </td>
+                    <td class="user-name">от 
+                        ${this.prices[0] == null ? '0' : this.prices[0]}  
+                        ${(this.prices[1] == null) ? '' : 'до ' + this.prices[1]}
+                    </td>
+        `;
+        
+        this.parent.append(element);
+        }
+
+        
+    }
+
+    const checkboxRequiredRange1 = document.querySelector('.requiredRange1'),
+    checkboxRequiredRange2 = document.querySelector('.requiredRange2'),
+    checkboxRequiredRange3 = document.querySelector('.requiredRange3');
+    let currentValue = courses;
+
+    currentValue.forEach(item => {
+        new Courses(item.name, item.prices, '.admin .container table').render();
+    });
+
+    checkboxRequiredRange1.addEventListener('click', () => {
+      if (checkboxRequiredRange1.checked != true){
+          currentValue = sortRequiredRange1();
+          console.log(courses);
+          currentValue.forEach(item => {
+            new Courses(item.name, item.prices, '.admin .container table').render();
+        });
+      }
+
+    });
+
+
+
+
+
     console.log(sortRequiredRange3());
